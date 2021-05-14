29125d8ed57347dce35f3790a470300c733d1996
Mike Kazantsev <mk.fraggod@gmail.com>
2021-05-15 00:37:16 +0500

    tests.get_peak_sample: use prng output for more consistent result

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError: readme = ''
 setup(
 
 	name = 'pulsectl',
-	version = '21.5.7',
+	version = '21.5.8',
 	author = 'George Filipkin, Mike Kazantsev',
 	author_email = 'mk.fraggod@gmail.com',
 	license = 'MIT',
