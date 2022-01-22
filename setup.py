ee69ec6b13b703a5de21b1334a288e465ff6cbe5
Mike Kazantsev <mk.fraggod@gmail.com>
2022-01-23 02:41:27 +0500

    tests: add python shebang so that it can be used as +x script

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError: readme = ''
 setup(
 
 	name = 'pulsectl',
-	version = '22.1.1',
+	version = '22.1.2',
 	author = 'George Filipkin, Mike Kazantsev',
 	author_email = 'mk.fraggod@gmail.com',
 	license = 'MIT',
